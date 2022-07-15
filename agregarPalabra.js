d7d40c032c56278d445458f924abf2abd8b89b46
Pablo Hugo de Dominicis <101358175+PabloHugo@users.noreply.github.com>
2022-07-15 17:21:06 -0300

    Add files via upload

---
+++ agregarPalabra.js
@@ -0,0 +1,15 @@
+   
+function guardarYcomenzar(){
+
+   var newWordInput = document.getElementById('inputText').value;
+   
+   wordList.push (newWordInput.toUpperCase());
+ 
+   window.location.href = "gameSprint2.html";
+    
+}
+    
+    
+    
+    
+ 

3a8eb02b6f0bcea1140a72630483f5c01a9338cf
Pablo Hugo de Dominicis <101358175+PabloHugo@users.noreply.github.com>
2022-08-05 17:41:43 -0300

    Delete palabras.js

--- palabras.js
+++
@@ -1,8 +0,0 @@
-
-var wordList = ["FELIZ","AHORCADO","HISTORIA","JUEGOS","ROBOTICA","CAMPANA", "HTML", "LOGICA", "LIBROS", "ALURA", "ORACLE"];
-
-var validLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"]; 
-
-
-
-
